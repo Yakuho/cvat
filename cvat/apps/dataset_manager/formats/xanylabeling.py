1e5ad0a799e766ed05e70c9ff53229e63db5618d
Yakuho <yakuho@foxmail.com>
2026-03-03 14:42:20 +0800

    Add xanylabeling import func and optimize export func

--- cvat/apps/dataset_manager/formats/xanylabeling.py
+++ cvat/apps/dataset_manager/formats/xanylabeling.py
@@ -4,123 +4,222 @@
 # SPDX-License-Identifier: MIT
 
 import os
-import os.path as osp
-import lxml.etree as ET
 import json
+import cv2
+import numpy as np
 
 from pathlib import Path
+from pyunpack import Archive
 from cvat.apps.dataset_manager.util import make_zip_archive
-from cvat.apps.dataset_manager.formats.cvat import dump_media_files, dump_task_or_job_anno, dump_as_cvat_annotation
+from cvat.apps.dataset_manager.formats.cvat import dump_media_files
+from cvat.apps.dataset_manager.bindings import TaskData
 from .registry import dm_env, exporter, importer
 
 
-class CVATAnnotation:
-    def __init__(self, path, dst=None, version="3.3.1"):
-        """
+class Dataset:
+    """This Object is used to capability of datumaro.Dataset, but not full-match yet temporary"""
+    def __init__(self, temp_dir: str):
+        # name mapping for fixing string decode error when contain like chinese characters
+        self.temp_dir = temp_dir
+        self._files = dict((
+            str(f)[len(temp_dir) + 1:].encode("cp437").decode("gbk").rsplit('.', 1)[0], f)
+            for f in Path(temp_dir).glob("**/*.json"))
 
-        :param path: cvat annotation xml
-        :param dst:  save dir
-        """
-        tree = ET.parse(path)
-        self.version = version
-        self.root = tree.getroot()
-        self.dst = dst
-        if self.dst is None:
-            self.dst = str(Path(path).parent)
+    def get_annatations(self, filepath: str):
+        """get xanylabeling shapes from json file (notice: no suffix)"""
+        annatations = []
+        if filepath in self._files:
+            with open(self._files[filepath], encoding="utf-8") as f:
+                annatations = json.load(f)["shapes"]
+        return annatations
 
-    def toxanylabeling(self):
-        _TYPE_MAP = {"polyline": "linestrip", "box": "rectangle", "points": "point"}
 
-        for annotation in self.root.findall("image"):
-            shapes = []
-            groups_id = [0]  # points group  TODO: Not fully compatible
-            for shape in annotation.findall("*"):
-                if shape.tag == "box":
-                    x0, y0, x1, y1 = shape.attrib["xtl"], shape.attrib["ytl"], shape.attrib["xbr"], shape.attrib["ybr"]
-                    x0, y0, x1, y1 = list(map(float, [x0, y0, x1, y1]))
-                    points = [[x0, y0], [x1, y0], [x1, y1], [x0, y1]]
-                elif shape.tag == "ellipse":
-                    continue # xanylabeling not support ellipse default
-                else:
-                    points = [list(map(float, xy.split(","))) for xy in shape.attrib["points"].split(";")]
+class XAnyLabelingBase:
+    _TYPE_MAP = {"polyline": "linestrip", "box": "rectangle", "points": "point"}
+    _SUPPORT_TYPES = ("box", "polygon", "rectangle", "polyline", "points")  # unsupported: ellipse, cuboid, mask
 
-                if shape.tag == "points":
-                    group_id = None
-                    if len(points)> 1:
-                        group_id = groups_id[0]
-                        groups_id[0] = groups_id[0] + 1
-                    for point in points:
-                        shapes.append({
-                            "kie_linking": [],
-                            "label": shape.attrib["label"],
-                            "score": None,
-                            "points": [point],
-                            "group_id": group_id,
-                            "description": "",
-                            "difficult": False,
-                            "shape_type": _TYPE_MAP.get(shape.tag, shape.tag),
-                            "flags": {},
-                            "attributes": {}
-                        })
-                else:
-                    shapes.append({
-                        "kie_linking": [],
-                        "label": shape.attrib["label"],
-                        "score": None,
-                        "points": points,
-                        "group_id": None,
-                        "description": "",
-                        "difficult": False,
-                        "shape_type": _TYPE_MAP.get(shape.tag, shape.tag),
-                        "flags": {},
-                        "attributes": {}
-                    })
+    def _export_shapes_iters(self, frame_annotation):
+        """get xanylabeling shapes from cvat frame_annotation"""
+        groups_id = {}  # record shape type group id for capability
+        for shape in frame_annotation.labeled_shapes:
+            if shape.type not in self._SUPPORT_TYPES:
+                continue
 
-            label_data = {
-                "version": self.version,
+            if shape.type in ("box", "rectangle"):
+                x0, y0, x1, y1 = list(shape.points)
+                points = [[x0, y0], [x1, y0], [x1, y1], [x0, y1]]
+            else:
+                it = iter(shape.points)
+                points = [list(pair) for pair in zip(it, it)]
+
+            shape_base = {
+                "kie_linking": [],
+                "label": shape.label,
+                "score": None,
+                "points": points,
+                "group_id": str(shape.group) if shape.group else None,  # default=0
+                "description": "",
+                "difficult": False,
+                "shape_type": self._TYPE_MAP.get(
+                    shape.type, shape.type),
                 "flags": {},
-                "shapes": shapes,
-                "imagePath": annotation.attrib["name"],
-                "imageData": None,
-                "imageHeight": int(annotation.attrib["height"]),
-                "imageWidth": int(annotation.attrib["width"]),
-                "description": ""
+                "attributes": {}
             }
 
-            savefile = Path(self.dst) / (annotation.attrib["name"].rsplit(".", 1)[0] + ".json")
+            if shape.type == "points" and len(points) > 1:
+                group_id = groups_id.get(shape.type, 0)
+                groups_id[shape.type] = group_id + 1
+                group_name = "points-{}".format(group_id)
+                if isinstance(shape_base["group_id"], str):
+                    group_name = "{}-{}".format(shape_base["group_id"], group_name)
+                shape_base.update({"group_id": group_name})
+                for point in points:
+                    yield {**shape_base, "points": [point]}
+            elif shape.type in ("box", "rectangle") and shape.rotation != 0:
+                center_point = np.mean(points, axis=0)
+                # negative angle for opencv clockwise and cvat angle is positive angle absolutaly
+                m = cv2.getRotationMatrix2D(tuple(center_point), -shape.rotation, 1.0)
+                shape_base.update({
+                    "shape_type": "rotation",
+                    "direction": np.deg2rad(shape.rotation),
+                    "points": cv2.transform(np.array(points).reshape(-1, 1, 2), m).reshape(-1, 2).tolist()
+                })
+                yield shape_base
+            else:
+                yield shape_base
+
+    def _export(self, temp_dir: str, instance_data: TaskData):
+        """convert format cvat to xanylabeling"""
+        for frame_annotation in instance_data.group_by_frame(include_empty=True):
+            savefile = Path(temp_dir) / (frame_annotation.name.rsplit(".", 1)[0] + ".json")
             os.makedirs(str(savefile.parent), exist_ok=True)
             with open(savefile, "w", encoding="utf-8") as f:
-                f.write(json.dumps(label_data, ensure_ascii=False, indent=2))
+                f.write(json.dumps({
+                    "version": self.VERSION,
+                    "flags": {},
+                    "shapes": list(self._export_shapes_iters(frame_annotation)),
+                    "imagePath": frame_annotation.name,
+                    "imageData": None,
+                    "imageHeight": int(frame_annotation.height),
+                    "imageWidth": int(frame_annotation.width),
+                    "description": ""
+                }, ensure_ascii=False, indent=2))
 
+    def _import_shapes_iters(self, shapes):
+        """get cvat frame_annotation from xanylabeling shapes"""
+        points_shape, groups = dict(), dict()
 
-def _export_images(dst_file, temp_dir, instance_data, save_images=False):
-    # create cvat annotation xml
-    annotation_file = osp.join(temp_dir, "annotations.xml")
-    with open(annotation_file, "wb") as f:
-        dump_task_or_job_anno(f, instance_data, dump_as_cvat_annotation)
+        # binding point shapes to cvat points when group_id the same
+        for _ in range(len(shapes)):
+            shape = shapes.pop(0)
+            if shape["shape_type"] == "point":
+                record = points_shape.get(shape["group_id"], [])
+                record.append(shape)
+                points_shape.update({shape["group_id"]: record})
+            else:
+                shapes.append(shape)
+        for group_id, points_group_shape in points_shape.items():
+            if group_id is None:  # no group
+                for shape in points_group_shape:
+                    shapes.append(shape)
+            else:
+                shape1st = {**points_group_shape[0]}  # 1st shape for template
+                shape1st.update({"points": [], "attributes": {}})
+                for shape in points_group_shape:
+                    shape1st["points"] += shape["points"]
+                if len(points_group_shape) > 1:
+                    shape1st.update({"group_id": None})
+                shapes.append(shape1st)
 
-    # convert cvat annotation to xanylabeling format
-    CVATAnnotation(annotation_file, temp_dir).toxanylabeling()
-    os.remove(annotation_file)
+        for shape in shapes:
+            if shape["shape_type"] in ("rectangle", "rotation"):
+                points = shape["points"]
+                assert len(points) in (2, 4), "rectangle shape must have 2 or 4 points"
+                if len(points) == 4:  # convert (left-top, right-top, right-bot, left-bot) to (left-top, right-bot)
+                    points = [points[i] for i in [0, 2]]
+                if shape.get("direction"):
+                    # negative angle for opencv clockwise and xanylabeling angle is positive angle absolutaly
+                    # xanylabeling rotation boxes xys is (rotation boxes), but cvat rotation boxes xys is (no rotation boxes)
+                    # so rotate xanylabeling boxes xys to no rotation boxes
+                    center_point = np.mean(points, axis=0)
+                    rotation = np.rad2deg(shape["direction"])
+                    m = cv2.getRotationMatrix2D(tuple(center_point), rotation, 1.0)
+                    points = cv2.transform(np.array(points).reshape(-1, 1, 2), m).reshape(-1, 2).tolist()
+            elif shape["shape_type"] == "circle":
+                ((cx, cy), (rx, ry)) = shape["points"]
+                radius = float(np.linalg.norm([rx - cx, ry - cy]))
+                points = [[cx, cy], [cx + radius, cy - radius]]
+            else:
+                points = shape["points"]
 
-    if save_images:
-        dump_media_files(instance_data, temp_dir)
+            group = 0
+            if shape["group_id"] is not None:
+                group = groups.get(shape["group_id"], len(groups)) + 1
+                groups[shape["group_id"]] = group
 
-    make_zip_archive(temp_dir, dst_file)
+            yield dict(
+                type=self._TYPE_MAP.get(shape["shape_type"], shape["shape_type"]),
+                rotation=round(np.rad2deg(shape["direction"]), 2) if shape.get("direction") else 0.0,
+                label=shape["label"],
+                points=[v for xy in points for v in xy],  # flatten points from [[x, y], ...]
+                attributes=[],
+                group=group
+            )
+
+    def _import(self, dataset: Dataset, instance_data: TaskData):
+        """convert format xanylabeling to cvat"""
+        for frame_annotation in instance_data.group_by_frame(include_empty=True):
+            shapes = dataset.get_annatations(frame_annotation.name.rsplit('.', 1)[0])  # query by full filepath with no suffix
+            for parsed_shape in self._import_shapes_iters(shapes):
+                instance_data.add_shape(instance_data.LabeledShape(
+                    **parsed_shape,
+                    z_order=0,
+                    source="auto",
+                    occluded=False,
+                    frame=frame_annotation.frame,
+                ))
 
 
 @exporter(name="XAnyLabeling", ext="ZIP", version="3.3.1")
-def _export(dst_file, temp_dir, instance_data, save_images=False):
-    _export_images(dst_file, temp_dir, instance_data, save_images)
+class XAnyLabelingExporter(XAnyLabelingBase):
+    """
+    XAnyLabelingExporter will be set attributes by exporter decorator:
+        self.NAME: XAnyLabeling
+        self.VERSION: 3.3.1
+        self.EXT: ZIP
+        self.DISPLAY_NAME: ...
+        self.DIMENSION: ...
+        self.ENABLED: ...
+    """
+    def __call__(self, dst_file, temp_dir: str, instance_data: TaskData, save_images: bool = False):
+        if save_images:
+            dump_media_files(instance_data, temp_dir)
+        self._export(temp_dir, instance_data)
+        make_zip_archive(temp_dir, dst_file)
 
 
-# @importer(name="xanylabeling", ext="ZIP", version="3.x")
-# def _import(src_file, temp_dir, instance_data, load_data_callback=None, **kwargs):
-#     Archive(src_file.name).extractall(temp_dir)
-#
-#     detect_dataset(temp_dir, format_name="label_me", importer=dm_env.importers.get("label_me"))
-#     dataset = Dataset.import_from(temp_dir, "label_me", env=dm_env)
-#     dataset = MaskToPolygonTransformation.convert_dataset(dataset, **kwargs)
-#     if load_data_callback is not None:
-#         load_data_callback(dataset, instance_data)
-#     import_dm_annotations(dataset, instance_data)
+@importer(name="xanylabeling", ext="ZIP", version="3.3.1")
+class XAnyLabelingImporter(XAnyLabelingBase):
+    """
+    XAnyLabelingImporter will be set attributes by importer decorator:
+        self.NAME: XAnyLabeling
+        self.VERSION: 3.3.1
+        self.EXT: ZIP
+        self.DISPLAY_NAME: ...
+        self.DIMENSION: ...
+        self.ENABLED: ...
+    """
+
+    _TYPE_MAP = {
+        **{v: k for k, v in XAnyLabelingBase._TYPE_MAP.items()},  # reverse kv
+        "circle": "ellipse", "rectangle": "rectangle", "rotation": "rectangle"
+    }
+
+    def __call__(self, src_file, temp_dir: str, instance_data: TaskData, load_data_callback=None, **kwargs):
+        # mask2poly = kwargs.get("conv_mask_to_poly", False)
+        Archive(src_file.name).extractall(temp_dir)
+        dataset = Dataset(temp_dir)
+        # dataset = MaskToPolygonTransformation.convert_dataset(dataset, **kwargs)
+        # if load_data_callback is not None:
+        #     load_data_callback(dataset, instance_data)
+        self._import(dataset, instance_data)
